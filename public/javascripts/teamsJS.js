27cac3ed1065b505bc953952cc3b620eff5378e0
Marc <Marc@DESKTOP-U8KSHSV.fritz.box>
2015-12-12 12:31:51 +0100

    Added images + teams league 2, ordered Tables, can show players to team

--- public/javascripts/teamsJS.js
+++ public/javascripts/teamsJS.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    populateTable();
+    populateTable(1);
 });
 
 function showteamdetails(shortname)
@@ -10,24 +10,57 @@ function showteamdetails(shortname)
         dataType: 'json'
     }).done(function(data){
         var players = data[0].players;
+        var playersOrdered = players.sort(function(a,b)
+        {
+            return b.score - a.score;
+        });
+        var tableContent = '';
+        //Clear the table before injecting the new players
+        $('#playertable table tbody').html(tableContent);
+        // For each item in our JSON, add a table row and cells to the content string
+        $.each(playersOrdered, function(){
+            tableContent += '<tr>';
+            tableContent += '<td>' + this.firstname + '</td>';
+            tableContent += '<td>' + this.lastname + '</td>';
+            tableContent += '<td>' + this.score + '</td>';
+            tableContent += '</tr>';
+        });
 
+        // Inject the whole content string into our existing HTML table
+        $('#playertable table tbody').html(tableContent);    
     });;
 }
 
 // Fill table with data
-function populateTable() {
+function populateTable(searchedleague) {
 
     // Empty content string
     var tableContent = '';
 
     // jQuery AJAX call for JSON
     $.getJSON( '/teams/list', function( data ) {
-
-        // For each item in our JSON, add a table row and cells to the content string
+        
+        var teamsOfRightLeague = [];
+        //we only need league one or two
         $.each(data, function(){
+            if(this.league === searchedleague)
+            {
+                teamsOfRightLeague.push(this);
+            }
+        });
+        
+
+        var teamsOrdered = teamsOfRightLeague.sort(function(a,b)
+        {
+            return b.points - a.points;
+        });
+        
+        // For each item in our JSON, add a table row and cells to the content string
+        $.each(teamsOrdered, function(){
             tableContent += '<tr>';
             tableContent += '<td><img src="' + this.icon + '"></td>';
             tableContent += '<td onclick=showteamdetails(' + "'" + this.shortname + "'" +')>' + this.teamname + '</td>';
+            tableContent += '<td>' + this.points + '</td>';
             tableContent += '</tr>';
         });
 
--- routes/teams.js
+++ routes/teams.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 //GET home page
 router.get('/', function(req,res,next){
-    res.render('teams', {title: 'teams'});
+    res.render('teams', {title: 'Teams'});
 });
 
 /* GET team listing. */
@@ -36,24 +36,42 @@ router.get('/write/fillDB', function(req, res){
         var col = db.collection('teams');
         col.insert(
                 [
-                    {teamname:"TSG 1899 Hoffenheim",shortname:"HOF",icon:"http://www.openligadb.de/images/teamicons/TSG_Hoffenheim.gif",players:getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"Eintracht Frankfurt",shortname:"EFR",icon:"http://www.openligadb.de/images/teamicons/Eintracht_Frankfurt.gif",players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"Hertha BSC",shortname:"BSC",icon:"http://www.openligadb.de/images/teamicons/Hertha_BSC.gif",players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"Borussia Dortmund",shortname:"BOR",icon:"http://www.openligadb.de/images/teamicons/Borussia_Dortmund.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"Werder Bremen",shortname:"WBR",icon:"http://www.openligadb.de/images/teamicons/werder_bremen.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"Hannover 96",shortname:"H69",icon:"http://www.openligadb.de/images/teamicons/Hannover_96.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"SC Freiburg",shortname:"SCF",icon:"http://www.openligadb.de/images/teamicons/SC_Freiburg.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"Hamburger SV",shortname:"HSV",icon:"http://www.openligadb.de/images/teamicons/Hamburger_SV.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"FC Schalke 04",shortname:"S04",icon:"http://www.openligadb.de/images/teamicons/FC_Schalke_04.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"1. FC Köln",shortname:"1FC",icon:"http://www.openligadb.de/images/teamicons/1_FC_Koeln.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"FC Augsburg",shortname:"FCA",icon:"http://www.openligadb.de/images/teamicons/FC_Augsburg.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"Bayern München",shortname:"BAY",icon:"http://www.openligadb.de/images/teamicons/Bayern_Muenchen.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"1. FSV Mainz 05",shortname:"FSV",icon:"http://www.openligadb.de/images/teamicons/1_FSV_Mainz_05.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"VfB Stuttgart",shortname:"VFB",icon:"http://www.openligadb.de/images/teamicons/VfB_Stuttgart.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"Bayer 04 Leverkusen",shortname:"B04",icon:"http://www.openligadb.de/images/teamicons/Bayer_Leverkusen.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"Borussia Mönchengladbach",shortname:"MGL",icon:"http://www.openligadb.de/images/teamicons/Bor_Moenchengladbach.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"VfL Wolfsburg",shortname:"VFL",icon:"http://www.openligadb.de/images/teamicons/VfL_Wolfsburg.gif", players: getRandomTeamMembers(), liga:1, punkte:0},
-                    {teamname:"SC Paderborn 07",shortname:"SCP",icon:"http://www.openligadb.de/images/teamicons/SC_Paderborn_07.gif", players: getRandomTeamMembers(), liga:1, punkte:0}
+                    {teamname:"TSG 1899 Hoffenheim",shortname:"HOF",icon:"images/TSG_Hoffenheim.gif",players:getRandomTeamMembers(), league:1, points:1},
+                    {teamname:"Eintracht Frankfurt",shortname:"EFR",icon:"images/Eintracht_Frankfurt.gif",players: getRandomTeamMembers(), league:1, points:5},
+                    {teamname:"Hertha BSC",shortname:"BSC",icon:"images/Hertha_BSC.gif",players: getRandomTeamMembers(), league:1, points:2},
+                    {teamname:"Borussia Dortmund",shortname:"BOR",icon:"images/Borussia_Dortmund.gif", players: getRandomTeamMembers(), league:1, points:13},
+                    {teamname:"Werder Bremen",shortname:"WBR",icon:"images/werder_bremen.gif", players: getRandomTeamMembers(), league:1, points:5},
+                    {teamname:"Hannover 96",shortname:"H69",icon:"images/Hannover_96.gif", players: getRandomTeamMembers(), league:1, points:13},
+                    {teamname:"FC Ingolstadt",shortname:"FCI",icon:"images/20px-FC-Ingolstadt_logo.svg.png", players: getRandomTeamMembers(), league:1, points:6},
+                    {teamname:"Hamburger SV",shortname:"HSV",icon:"images/Hamburger_SV.gif", players: getRandomTeamMembers(), league:1, points:9},
+                    {teamname:"FC Schalke 04",shortname:"S04",icon:"images/FC_Schalke_04.gif", players: getRandomTeamMembers(), league:1, points:4},
+                    {teamname:"1. FC Köln",shortname:"1FC",icon:"images/1_FC_Koeln.gif", players: getRandomTeamMembers(), league:1, points:0},
+                    {teamname:"FC Augsburg",shortname:"FCA",icon:"images/FC_Augsburg.gif", players: getRandomTeamMembers(), league:1, points:0},
+                    {teamname:"Bayern München",shortname:"BAY",icon:"images/Bayern_Muenchen.gif", players: getRandomTeamMembers(), league:1, points:0},
+                    {teamname:"1. FSV Mainz 05",shortname:"FSV",icon:"images/1_FSV_Mainz_05.gif", players: getRandomTeamMembers(), league:1, points:0},
+                    {teamname:"VfB Stuttgart",shortname:"VFB",icon:"images/VfB_Stuttgart.gif", players: getRandomTeamMembers(), league:1, points:0},
+                    {teamname:"Bayer 04 Leverkusen",shortname:"B04",icon:"images/Bayer_Leverkusen.gif", players: getRandomTeamMembers(), league:1, points:0},
+                    {teamname:"Borussia Mönchengladbach",shortname:"MGL",icon:"images/Bor_Moenchengladbach.gif", players: getRandomTeamMembers(), league:1, points:0},
+                    {teamname:"VfL Wolfsburg",shortname:"VFL",icon:"images/VfL_Wolfsburg.gif", players: getRandomTeamMembers(), league:1, points:0},
+                    {teamname:"SV Darmstadt 98",shortname:"SVD",icon:"images/SV_Darmstadt_98.gif", players: getRandomTeamMembers(), league:1, points:0},
+                    {teamname:"Karslruher SC",shortname:"KSC",icon:"images/Karlsruher_SC.gif",players:getRandomTeamMembers(), league:2, points:1},
+                    {teamname:"SV Sandhausen",shortname:"SVS",icon:"images/SV_Sandhausen.svg.png",players: getRandomTeamMembers(), league:2, points:5},
+                    {teamname:"VFL Bochum",shortname:"VFB",icon:"images/VfL_Bochum.gif",players: getRandomTeamMembers(), league:2, points:2},
+                    {teamname:"FC Union Berlin",shortname:"FCU",icon:"images/1_FC_Union_Berlin.gif", players: getRandomTeamMembers(), league:2, points:13},
+                    {teamname:"Arminia Bielefeld",shortname:"ARM",icon:"images/Arminia_Bielefeld.gif", players: getRandomTeamMembers(), league:2, points:5},
+                    {teamname:"Red Bull Leipzig",shortname:"RBL",icon:"images/1583.png", players: getRandomTeamMembers(), league:2, points:13},
+                    {teamname:"TSV 1860 München",shortname:"1860",icon:"images/TSV_1860_Muenchen.gif", players: getRandomTeamMembers(), league:2, points:6},
+                    {teamname:"FC St Pauli",shortname:"STP",icon:"images/FC_St_Pauli.gif", players: getRandomTeamMembers(), league:2, points:9},
+                    {teamname:"SC Freiburg",shortname:"SCF",icon:"images/SC_Freiburg.gif", players: getRandomTeamMembers(), league:2, points:4},
+                    {teamname:"SC Paderborn",shortname:"SCP",icon:"images/SC_Paderborn_07.gif", players: getRandomTeamMembers(), league:2, points:0},
+                    {teamname:"1 FC Heidenheim",shortname:"FCH",icon:"images/1-fc-heidenheim.png", players: getRandomTeamMembers(), league:2, points:0},
+                    {teamname:"SpVgg Greuther Fürth",shortname:"GRF",icon:"images/SpVgg_Greuther_Fuerth.gif", players: getRandomTeamMembers(), league:2, points:0},
+                    {teamname:"1. FC Kaiserslautern",shortname:"FSV",icon:"images/1_FC_Kaiserslautern.gif", players: getRandomTeamMembers(), league:2, points:0},
+                    {teamname:"FSV Frankfurt",shortname:"FSV",icon:"images/20px-FSV_Frankfurt_1899.svg.png", players: getRandomTeamMembers(), league:2, points:0},
+                    {teamname:"1 FC Nürnberg",shortname:"FCN",icon:"images/1_FC_Nuernberg.gif", players: getRandomTeamMembers(), league:2, points:0},
+                    {teamname:"Eintracht Braunschweig",shortname:"EBR",icon:"images/Eintracht_Braunschweig.gif", players: getRandomTeamMembers(), league:2, points:0},
+                    {teamname:"Fortuna Düsseldorf",shortname:"FOR",icon:"images/20x20_fortuna-duesseldorf.png", players: getRandomTeamMembers(), league:2, points:0},
+                    {teamname:"MSV Duisburg",shortname:"MSV",icon:"images/MSV_Duisburg.gif", players: getRandomTeamMembers(), league:2, points:0}
                 ],
                     function(err,data){
                         //handle error
@@ -72,9 +90,9 @@ function getRandomTeamMembers()
     {
         jsonArr.push(
                 {
-            name: getRandomString(),
-            vorname: getRandomString(),
-            score: 0
+            firstname: getRandomString(),
+            lastname: getRandomString(),
+            score: i
                 });           
     }    
     return jsonArr;  
