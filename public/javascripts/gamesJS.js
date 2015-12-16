b5c5be3be1cdbcac7d1ab17a6707187dbaf02b06
Marc <Marc@DESKTOP-U8KSHSV.fritz.box>
2015-12-16 19:09:03 +0100

    Rework on games DB structure. Simulate games with results and points for teams

--- public/javascripts/gamesJS.js
+++ public/javascripts/gamesJS.js
@@ -10,18 +10,24 @@ function populateTable()
         type: "GET",
         dataType: 'json'
     }).done(function(data){
-        var games = data[0].games;
-        var gamesOrderedByMatchday = games.sort(function(a,b)
+        var allGames = [];
+        
+        var gamesOrderedByMatchday = data.sort(function(a,b)
         {
-            return b.matchday - a.matchday;
+            return a.matchday - b.matchday;
         });
+        for(var i = 0; i < gamesOrderedByMatchday.length; i++)
+        {
+            allGames = allGames.concat(gamesOrderedByMatchday[i].games);
+        }
+
         var tableContent = '';
         //Clear the table before injecting the new players
         $('#gamestable table tbody').html(tableContent);
         // For each item in our JSON, add a table row and cells to the content string
         var i = 0;
         var matchday = 1;
-        $.each(gamesOrderedByMatchday, function(){
+        $.each(allGames, function(){
             if(i % 9 === 0)
             {
                 tableContent += '<tr><th class="middleTh"> Spieltag:' + matchday + '</th></tr>';
@@ -30,7 +36,17 @@ function populateTable()
             tableContent += '<tr>';
             tableContent += '<td>' + this.team1 + '</td>';
             tableContent += '<td>' + this.team2 + '</td>';
-            tableContent += '<td>' + this.goalsTeam1 + ":" + this.goalsTeam2 + '</td>';
+            if(this.goalsTeam1 === -1 ||this.goalsTeam2 === -1)
+            {
+                tableContent += '<td>' + "tbd" + ":" + "tbd" + '</td>';
+            }
+            else
+            {
+                tableContent += '<td>' + this.goalsTeam1 + ":" + this.goalsTeam2 + '</td>';
+            }
+            tableContent += '<td>' + this.location + '</td>';
+            tableContent += '<td>' + this.date + '</td>';
+            tableContent += '<td>' + this.saison + '</td>';
             tableContent += '</tr>';
             
             i++;
--- routes/games.js
+++ routes/games.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var dateOfMatchdayFirstHalf = new Date(2015, 7, 15, 15, 30);
+var dateOfMatchdaySecondHalf = new Date(2016, 1, 23, 15, 30);
 
 //GET home page
-router.get('/', function(req,res,next){
+router.get('/', function (req, res, next) {
     res.render('games', {title: 'Spiele'});
 });
 
 // Database 1
 //team1 team2 ort saison spieltag datum toreteam1 toreteam2
-router.get('/write/fillDB', function(req, res){
+router.get('/write/fillDB', function (req, res) {
     var db = req.db;
     var collection = db.get('teams');
-    collection.find({league: 1}, function(err, teams){
+    collection.find({league: 1}, function (err, teams) {
         fillDatabaseWithRandomGames(teams);
         res.send("DB erfolgreich gefüllt");
     });
@@ -19,10 +21,10 @@ router.get('/write/fillDB', function(req, res){
 });
 
 /* GET game listing. */
-router.get('/list', function(req, res) {
+router.get('/list', function (req, res) {
     var db = req.db;
     var collection = db.get('games');
-    collection.find({},{},function(e,docs){
+    collection.find({}, {}, function (e, docs) {
         res.json(docs);
     });
 });
@@ -30,52 +32,167 @@ router.get('/list', function(req, res) {
 function fillDatabaseWithRandomGames(arrayWithTeams)
 {
     var mongo = require('mongodb').MongoClient;
-    mongo.connect('mongodb://localhost:27017/fussballApp', function(err, db){
+    mongo.connect('mongodb://localhost:27017/fussballApp', function (err, db) {
         console.log("Connected properly.");
         var col = db.collection('games');
-        col.insert(
-                [
-                
-                {games:getGamesJson(arrayWithTeams)}
-                    
-                ],
-                    function(err,data){
+
+        for (var i = 1; i <= 34; i++)
+        {
+            col.insert(
+                    [
+                        {matchday: i,
+                            games: getGamesJson(arrayWithTeams, i)
+                        }
+
+                    ],
+                    function (err, data) {
                         //handle error
                     }
-                );
+            );
+
+        }
+
+
+
         db.close();
     });
     console.log('Erfolgreich eingefügt');
 }
 
-function getGamesJson(arrayWithTeams)
+//Returns one Matchday of Games
+function getGamesJson(arrayWithTeams, matchday)
 {
-    
+ //TODO FUNKTIONIERT NICHT RICHTIG
     var jsonArr = [];
-    
-    for(var j = 1; j <= 17; j++)
+    if(matchday === 1)
     {
-        for(var i = 0; i < 18; i += 2)
-        {
-        jsonArr.push(
-                {
-            team1: arrayWithTeams[i].teamname,
-            team2: arrayWithTeams[(i+j) % 18].teamname,
-            matchday: j,
-            location: "ka",
-            saison: "2015",
-            date: "ka",
-            goalsTeam1: getRandomResult(),
-            goalsTeam2: getRandomResult()
-                });  
-        } 
+        dateOfMatchdayFirstHalf = new Date(2015, 7, 15, 15, 30);
+        dateOfMatchdaySecondHalf = new Date(2016, 1, 23, 15, 30);
     }
-   
-    return jsonArr;  
+    
+    if (matchday <= 17)
+    {
+        for (var i = 0; i < 18; i += 2)
+        {
+            //Hinspiele
+            jsonArr.push(
+                    {                 
+                        team1: arrayWithTeams[i].teamname,
+                        team2: arrayWithTeams[(i + matchday) % 18].teamname,
+                        location: arrayWithTeams[i].location,
+                        saison: dateOfMatchdayFirstHalf.getFullYear() + "/" + dateOfMatchdaySecondHalf.getFullYear(),
+                        date: dateOfMatchdayFirstHalf.toString(),
+                        goalsTeam1: -1,
+                        goalsTeam2: -1
+                    });
+        }
+        //add one week after all matches are played
+        dateOfMatchdayFirstHalf.setDate((dateOfMatchdayFirstHalf.getDate() + 7));
+    }
+    else if(matchday > 18)
+    {      
+        for (var i = 0; i < 18; i += 2)
+        {
+        //Hinspiele
+        jsonArr.push(
+                {                 
+                    team1: arrayWithTeams[(i + matchday) % 18].teamname,
+                    team2: arrayWithTeams[i].teamname,
+                    location: arrayWithTeams[(i + matchday) % 18].location,
+                    saison: dateOfMatchdayFirstHalf.getFullYear() + "/" + dateOfMatchdaySecondHalf.getFullYear(),
+                    date: dateOfMatchdaySecondHalf.toString(),
+                    goalsTeam1: -1,
+                    goalsTeam2: -1
+                });
+        } 
+        //add one week after all matches are played
+        dateOfMatchdaySecondHalf.setDate((dateOfMatchdaySecondHalf.getDate() + 7));
+    }
+    return jsonArr;
 }
 
 function getRandomResult()
 {
-    return Math.floor((Math.random() * 5) + 1);
+    return Math.floor((Math.random() * 5));
 }
+
+/* GET simulate Matchday */
+router.get('/simulate', function (req, res) {
+    var db = req.db;
+    var collection = db.get('games');
+    collection.find({}, {}, function (e, everyMatchdayGames) {
+        updateGames(everyMatchdayGames, req, res);
+    });
+});
+
+function updateGames(everyMatchdayGames, req, res)
+{
+    var gamesOrderedByMatchday = everyMatchdayGames.sort(function (a, b)
+    {
+        return a.matchday - b.matchday;
+    });
+
+    var actualMatchDay = 0;
+    matchDayFound = false;
+    var i = 0;
+    while (i < 34 && !matchDayFound)
+    {
+        //Er wird geschaut welcher SPieltag als nächstes eingetragen werden muss
+        if (gamesOrderedByMatchday[i].games[0].goalsTeam1 === -1)
+        {
+            actualMatchDay = gamesOrderedByMatchday[i].matchday;
+            matchDayFound = true;
+        }
+        i++;
+    }
+
+    var db = req.db;
+    var collection = db.get('games');
+    
+    collection.update({ matchday: actualMatchDay }, { "$set": { 
+            "games.0.goalsTeam1": getRandomResult(),
+            "games.0.goalsTeam2": getRandomResult(),
+            "games.1.goalsTeam1": getRandomResult(),
+            "games.1.goalsTeam2": getRandomResult(),
+            "games.2.goalsTeam1": getRandomResult(),
+            "games.2.goalsTeam2": getRandomResult(),
+            "games.3.goalsTeam1": getRandomResult(),
+            "games.3.goalsTeam2": getRandomResult(),
+            "games.4.goalsTeam1": getRandomResult(),
+            "games.4.goalsTeam2": getRandomResult(),
+            "games.5.goalsTeam1": getRandomResult(),
+            "games.5.goalsTeam2": getRandomResult(),
+            "games.6.goalsTeam1": getRandomResult(),
+            "games.6.goalsTeam2": getRandomResult(),
+            "games.7.goalsTeam1": getRandomResult(),
+            "games.7.goalsTeam2": getRandomResult(),
+            "games.8.goalsTeam1": getRandomResult(),
+            "games.8.goalsTeam2": getRandomResult()        
+        } });
+    
+    
+    collection.find({ matchday: actualMatchDay }, function (e, games) {
+        var matchdayGames = games[0].games;
+        for (var i = 0; i < matchdayGames.length; i++)
+        {
+            if (matchdayGames[i].goalsTeam1 > matchdayGames[i].goalsTeam2)
+            {
+                    db.get('teams').update({ teamname: matchdayGames[i].team1 }, { "$inc": { points: 3 } });
+            }
+            else if (matchdayGames[i].goalsTeam1 < matchdayGames[i].goalsTeam2)
+            {
+                    db.get('teams').update({ teamname: matchdayGames[i].team2 }, { "$inc": { points: 3 } });
+            }
+            else if (matchdayGames[i].goalsTeam1 === matchdayGames[i].goalsTeam2)
+            {
+                    db.get('teams').update({ teamname: matchdayGames[i].team1 }, { "$inc": { points: 1 } });
+                    db.get('teams').update({ teamname: matchdayGames[i].team2 }, { "$inc": { points: 1 } });
+            }	 
+        }
+    });
+
+    
+    res.send("Erfolgreich geändert");
+}
+
 module.exports = router;
